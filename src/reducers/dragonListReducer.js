2fcc570f19f7b5e997eff08c33e6dfd9bb4ee0b2
Edison Jeon <edisonthe2nd@gmail.com>
2023-12-22 14:21:58 -0800

    title functionality not working, also added a dragon member randomness in reducer

--- src/reducers/dragonListReducer.js
+++ src/reducers/dragonListReducer.js
@@ -1,20 +1,24 @@
 const initialState = {
-    members: [
-      { name: 'Jojo Zhang', dragonStatus: true },
-      { name: 'Brandon Harris', dragonStatus: false }
-    ]
-  };
-  
-  export const dragonListReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case 'ADD_MEMBER':
-        const newMember = { name: action.payload, dragonStatus: false };
-        return {
-          ...state,
-          members: [...state.members, newMember]
-        };
-      default:
-        return state;
-    }
-  };
-  
+  members: [
+    { name: "Jojo Zhang", dragonStatus: true },
+    { name: "Brandon Harris", dragonStatus: false },
+  ],
+};
+
+export const dragonListReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_MEMBER":
+      const luckyNumber = Math.round(Math.random());
+      console.log(luckyNumber);
+      const newMember = {
+        name: action.payload,
+        dragonStatus: luckyNumber === 1 ? true : false,
+      };
+      return {
+        ...state,
+        members: [...state.members, newMember],
+      };
+    default:
+      return state;
+  }
+};
